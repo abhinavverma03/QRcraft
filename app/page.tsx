5973156838b58d00ced2d6b595a905e3e334e13f
Abhinav Verma <mudunuriabhi@gmail.com>
2025-07-06 03:16:11 +0530

    Disable toggle dark mode button

--- app/page.tsx
+++ app/page.tsx
@@ -130,13 +130,13 @@ export default function Home() {
                 <Shield className="w-4 h-4" />
                 <span>Secure & Fast</span>
               </div>
-              <button
+              {/* <button
                 onClick={toggleDarkMode}
                 className="p-2 rounded-lg bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors duration-200"
                 aria-label="Toggle dark mode"
               >
                 {darkMode ? <Sun className="w-5 h-5 text-yellow-500" /> : <Moon className="w-5 h-5 text-gray-600" />}
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
